8461b91cc7d9740a77c1f3939f5b9dfd110b750e
AFournier-Dev <alexandre.fournier@yahoo.fr>
2021-02-05 10:42:01 +0100

    logo better WIP

--- header.php
+++ header.php
@@ -14,23 +14,32 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-4">
         <div class="container-fluid">
 
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1508 89">
+
+            <svg xmlns="http://www.w3.org/2000/svg" width="775" height="123" viewBox="0 0 775 123">
                 <a href=<?= get_home_url() ?>>
                     <style type="text/css">
                         a:link {
                             text-decoration: none;
                         }
                     </style>
-                    <text id="APPRENDRE_COMMENCER" data-name="APPRENDRE &amp; COMMENCER" transform="translate(0 69)" fill="#0800e8" font-size="72" font-family="BodoniMT, Bodoni MT">
-                        <tspan x="0" y="0">A</tspan>
-                        <tspan y="0" font-size="58" font-family="FuturaBT-Light, Futura Lt BT" font-weight="300" fill="#000">PPRENDRE</tspan>
-                        <tspan y="0" font-size="58" fill="#000"> </tspan>
-                        <tspan y="0" font-family="FuturaBT-Light, Futura Lt BT" font-weight="300" fill="#000">&amp;</tspan>
-                        <tspan y="0" font-size="58" fill="#000"> </tspan>
-                        <tspan y="0" fill="#fc0d0b">C</tspan>
-                        <tspan y="0" font-size="58" font-family="FuturaBT-Light, Futura Lt BT" font-weight="300" fill="#000">OMMENCER</tspan>
-                    </text>
+                    <g id="Groupe_6" data-name="Groupe 6" transform="translate(-928 -213)">
+                        <text id="PPRENDRE_OMMENCER" data-name="PPRENDRE  &amp;    OMMENCER" transform="translate(980 294)" font-size="58" font-family="FuturaBT-Light, Futura Lt BT" font-weight="300">
+                            <tspan x="0" y="0" letter-spacing="-0.005em">P</tspan>
+                            <tspan y="0">PRENDRE</tspan>
+                            <tspan y="0" xml:space="preserve" font-family="BodoniMT, Bodoni MT" font-weight="400"> </tspan>
+                            <tspan y="0" font-size="72">&amp; </tspan>
+                            <tspan y="0" xml:space="preserve" font-family="BodoniMT, Bodoni MT" font-weight="400"> </tspan>
+                            <tspan y="0">OMMENCER</tspan>
+                        </text>
+                        <text id="C" transform="translate(1338 307)" fill="#fc0d0b" font-size="102" font-family="BodoniMT, Bodoni MT">
+                            <tspan x="0" y="0">C</tspan>
+                        </text>
+                        <text id="A" transform="translate(928 307)" fill="#0800e8" font-size="102" font-family="BodoniMT, Bodoni MT">
+                            <tspan x="0" y="0">A</tspan>
+                        </text>
+                    </g>
             </svg>
+
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
