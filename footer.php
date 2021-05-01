57e1128cb92a7183c0e012d5652ddba6b08ba0be
AFournier-Dev <alexandre.fournier@yahoo.fr>
2021-05-01 16:00:13 +0200

    mis en page / implementation de photos ... WIP

--- footer.php
+++ footer.php
@@ -1,14 +1,13 @@
 </div>
 
 
-
-<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #1c1f23;">
+<nav class="navbar-nav navbar-expand-lg navbar-dark" style="background-color: #1c1f23;">
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
 
 
-    <div class="container-fluid">
+    <div class="container-fluid ">
         <footer class="footer" id="footer">
             <div class="col-sm-10 mx-auto">
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">                    
@@ -26,6 +25,8 @@
         </footer>
     </div>
 </nav>
+
+
 <?php wp_footer() ?>
 <!--permet d'inclure toutes les fonctions dans le footer -->
 </body>
